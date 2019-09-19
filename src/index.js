08400dfb058cb825a18ed1297824da5651b016d5
Katrina Roaix <kroaix@gmail.com>
2019-09-18 22:38:08 -0400

    Wrapped App in ProductContext.Provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './sass/index.scss';
 import App from './App';
+import ProductContext from './contexts/ProductContext';
 
 const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-	<Router>
-		<AppWithRouter />
-	</Router>,
-	document.getElementById('root')
+  <ProductContext.Provider>
+    <Router>
+      <AppWithRouter />
+    </Router>
+  </ProductContext.Provider>,
+  document.getElementById("root")
 );
